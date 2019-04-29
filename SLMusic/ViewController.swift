1e83789e27f0caa13ae8e1b2c18c0811a9c1dd20
joakim lundberg <joakim.lundb@gmail.com>
2019-04-29 14:37:05 +0200

    fix logo

--- SLMusic/ViewController.swift
+++ SLMusic/ViewController.swift
@@ -10,35 +10,17 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var label: UILabel!
-
-    @IBOutlet weak var textField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setBorderAndRadius()
-        label.text = "Välkommen! Tryck på knappen"
+       // setBorderAndRadius()
+       // label.text = "Välkommen! Tryck på knappen"
         
     }
     
-    func setBorderAndRadius() {
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.magenta.cgColor
-        button.layer.cornerRadius = 25
+
     }
 
-    @IBAction func button(_ sender: UIButton) {
-        if textField.text == "" {
-            label.text = "Du bör skriva något"
-        } else {
-        label.text = textField.text
-            textField.text = ""
-        }
-    }
-    @IBAction func goToSettings(_ sender: Any) {
-        performSegue(withIdentifier: "goToSettingsVC", sender: self)
-    }
-    
-}
+
 
