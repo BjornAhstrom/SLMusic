a6352499b0b3ce60f4ea5f648e885fc08a5be87c
Björn Åhström <ahstrom_81@hotmail.com>
2019-04-30 12:46:56 +0200

    Created view controller and tableView cell

---
+++ SLMusic/Trip View Controller Björn/TripCell.swift
@@ -0,0 +1,19 @@
+//
+//  TripCell.swift
+//  SLMusic
+//
+//  Created by Björn Åhström on 2019-04-30.
+//  Copyright © 2019 Björn Åhström. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TripCell: UITableViewCell {
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var departureTimeLabel: UILabel!
+    @IBOutlet weak var departureStationLabel: UILabel!
+    @IBOutlet weak var tripLenghtLabel: UILabel!
+    @IBOutlet weak var arrivalTimeLabel: UILabel!
+    @IBOutlet weak var arrivalStationLabel: UILabel!
+}
